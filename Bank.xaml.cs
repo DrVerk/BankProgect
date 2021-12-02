6b01ad1b38bbde225eb81b1d078f3181eaff6196
DrVerk <matvejebel8@gmail.com>
2021-12-02 19:28:32 +0700

    Шлифовка1

--- Bank.xaml.cs
+++ Bank.xaml.cs
@@ -14,17 +14,14 @@ namespace BankProgect
             InitializeComponent();
             DataContext = bank;
         }
-
         private void Taib_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
             bank.Acount = (User<Account>)Taib.SelectedItem;
         }
-
         private void AccountChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
             bank.account = (Account)AccountsView.SelectedItem;
         }
-
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
             if (bank.DepOr)
@@ -40,37 +37,31 @@ namespace BankProgect
                 dEP.Visibility = Visibility.Hidden;
             }
         }
-
         private void ComboBox_UserChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             if (UsCombo.SelectedItem != null)
                 bank.Acount = (User<Account>)UsCombo.SelectedItem;
         }
-
         private void AcCombo_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             if (AcCombo.SelectedItem != null)
                 bank.account = (Account)AcCombo.SelectedItem;
         }
-
         private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             if (Ac2.SelectedItem != null)
                 bank.account1 = (Account)Ac2.SelectedItem;
         }
-
         private void ComboBox_SelectionChanged_1(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             if (Ac1.SelectedItem != null)
                 bank.account = (Account)Ac1.SelectedItem;
         }
-
         private void ComboBox_SelectionChanged_2(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             if (Sc1.SelectedItem != null)
                 bank.Acount1 = (User<Account>)Sc1.SelectedItem;
         }
-
         private void ComboBox_SelectionChanged_3(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             if (Sc2.SelectedItem != null)
